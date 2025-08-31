9d26c7082d8777bd0653e526a80c64ce45108e9f
amina-nur <aminamumin606@gmail.com>
2025-08-31 12:09:22 +0300

    Update App.js with FocusMate layout and logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import logo from "./focusMate.png";
+import "./App.css";
+
+// Importing components
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
+import PlantVisualizer from "./components/PlantVisualizer";
+import MotivationalQuote from "./components/MotivationalQuote";
+import PomodoroTimer from "./components/PomodoroTimer";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FocusMate</h1>
       </header>
+      <main>
+        <MotivationalQuote />
+        <AddTask addTask={addTask} />
+        <TodoList tasks={tasks} removeTask={removeTask} />
+        <PlantVisualizer tasks={tasks} />
+        <PomodoroTimer />
+      </main>
+      <footer>
+        <p>&copy; 2025 FocusMate. All rights reserved.</p>
+      </footer>
     </div>
   );
+
 }
 
 export default App;
