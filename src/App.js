3f08fd3ea0cfc0557083d31f9ff9f9a145f80c07
amina-nur <aminamumin606@gmail.com>
2025-09-09 23:40:25 +0300

    Simplify home route, remove inline style to fix blank page

--- src/App.js
+++ src/App.js
@@ -59,17 +59,7 @@ function App() {
       <Route
   path="/"
   element={
-    <div
-      style={{
-        backgroundColor: 'pink',
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "2rem",
-        color: "white"
-      }}
-    >
+    <div>
       <h1>Welcome to FocusMate!</h1>
     </div>
   }
